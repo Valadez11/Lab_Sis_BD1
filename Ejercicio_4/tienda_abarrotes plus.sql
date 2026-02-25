49ccf104ed5144c2df2f5ffc26eba403daf01b09
Alfonso Valadez <alfonso.2000.ava@gmail.com>
2026-02-25 11:14:35 -0600

    UPGRADE REPO

---
+++ Ejercicio_4/tienda_abarrotes plus.sql
@@ -0,0 +1,147 @@
+CREATE TABLE Clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR (50) NOT NULL,
+    telefono VARCHAR(15),
+    direccion VARCHAR(100),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    fecha_registro DATE NOT NULL
+
+);
+
+CREATE TABLE empleados(
+    id_empleado INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15),
+    puesto VARCHAR(50) NOT NULL,
+    fecha_contratacion DATE NOT NULL,
+    activo BOOLEAN NOT NULL DEFAULT TRUE
+
+);
+
+CREATE TABLE proveedores (
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(15),
+    empresa VARCHAR(100)NOT NULL
+);
+
+CREATE TABLE categorias(
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(200) 
+);
+
+CREATE TABLE productos (
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_prodcuto VARCHAR(100) NOT NULL,
+    precio_venta DECIMAL(10, 2) NOT NULL,
+    precio_compra DECIMAL(10, 2) NOT NULL,
+    desccripcion VARCHAR(200),
+    id_categoria INT,
+    id_proveedor INT,
+    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
+);
+
+CREATE TABLE ventas (
+    id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    total DECIMAL(10, 2)NOT NULL,
+    fecha_venta DATE NOT NULL,
+    id_cliente INT,
+    id_empleado INT,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+    FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado)
+);
+
+CREATE TABLE compras (
+    id_compra INT AUTO_INCREMENT PRIMARY KEY,
+    total DECIMAL(10, 2) NOT NULL,
+    fecha_compra DATE NOT NULL,
+    id_proveedor INT,
+
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
+);
+
+
+CREATE TRIGGER trg_detalle_compra_insert
+BEFORE INSERT ON detalle_compra
+FOR EACH ROW
+BEGIN
+    SET NEW.subtotal = NEW.cantidad * NEW.precio_compra;
+END $$
+
+
+CREATE TRIGGER trg_aumentar_stock
+AFTER INSERT ON detalle_compra
+FOR EACH ROW
+BEGIN
+    UPDATE productos
+    SET stock = stock + NEW.cantidad
+    WHERE id_producto = NEW.id_producto;
+END $$
+
+CREATE TRIGGER trg_total_compra
+AFTER INSERT ON detalle_compra
+FOR EACH ROW
+BEGIN
+    UPDATE compras
+    SET total = (
+        SELECT SUM(subtotal)
+        FROM detalle_compra
+        WHERE id_compra = NEW.id_compra
+    )
+    WHERE id_compra = NEW.id_compra;
+END $$
+
+
+CREATE TRIGGER trg_detalle_venta_insert
+BEFORE INSERT ON detalle_venta
+FOR EACH ROW
+BEGIN
+    SET NEW.subtotal = NEW.cantidad * NEW.precio_unitario;
+END $$
+
+
+CREATE TRIGGER trg_disminuir_stock
+AFTER INSERT ON detalle_venta
+FOR EACH ROW
+BEGIN
+    UPDATE productos
+    SET stock = stock - NEW.cantidad
+    WHERE id_producto = NEW.id_producto;
+END $$
+
+
+
+CREATE TRIGGER trg_total_venta
+AFTER INSERT ON detalle_venta
+FOR EACH ROW
+BEGIN
+    UPDATE ventas
+    SET total = (
+        SELECT SUM(subtotal)
+        FROM detalle_venta
+        WHERE id_venta = NEW.id_venta
+    )
+    WHERE id_venta = NEW.id_venta;
+END $$
+
+
+
+CREATE TRIGGER trg_validar_stock
+BEFORE INSERT ON detalle_venta
+FOR EACH ROW
+BEGIN
+    DECLARE stock_actual INT;
+
+    SELECT stock INTO stock_actual
+    FROM productos
+    WHERE id_producto = NEW.id_producto;
+
+    IF stock_actual < NEW.cantidad THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Stock insuficiente para realizar la venta';
+    END IF;
+END $$
---
+++ Ejercicio_5/Querry inserción de datos.txt
@@ -0,0 +1,63 @@
+Querry inserción de datos
+
+INSERT INTO autor(nombre, nacionalidad, fecha_nacimiento) VALUES
+('Gabriel García Márquez', 'Colombiana', '1927-03-06'),
+('Isable Gonzales', 'Española', '1950-07-15'),
+('J.K. Rowling', 'Británica', '1965-07-31');
+
+INSERT INTO editorial (nombre, pais, telefono) VALUES
+('Editorial Planeta','España','5551112233'),
+('Penguin Random House','Estados Unidos','5552223344'),
+('Editorial Sudamericana', 'Argentina', '5553334455');
+
+INSERT INTO libro (titulo, genero, id_autor)
+SELECT 'Cien años de soledad','Realismo mágico', a.id_autor
+FROM autor a WHERE a.nombre = 'Gabriel García Márquez'
+UNION ALL
+SELECT 'Harry Potter y la piedra filosofal','Fantasía', a.id_autor
+FROM autor a WHERE a.nombre = 'J.K. Rowling'
+UNION ALL
+SELECT '1984','Distopía', a.id_autor
+FROM autor a WHERE a.nombre = 'George Orwell';
+
+
+INSERT INTO edicion (id_libro, id_editorial, anio_publicacion, isbn, numero_edicion) VALUES
+(1,1,2001,'ISBN-1111',1),
+(2,2,1997,'ISBN-2222',1),
+(3,2,1949,'ISBN-3333',1),
+(1,2,2010,'ISBN-4444',2);
+
+INSERT INTO ejemplar (id_edicion, codigo_barras, estado, ubicacion) VALUES
+(1,'CB001','Disponible','Estante A1'),
+(1,'CB002','Disponible','Estante A1'),
+(2,'CB003','Disponible','Estante B2'),
+(3,'CB004','Disponible','Estante C3'),
+(4,'CB005','Disponible','Estante A2');
+
+INSERT INTO usuario (nombre, correo, telefono, fecha_registro) VALUES
+('Juan Pérez','juan@mail.com','5551110001','2026-01-10'),
+('Ana Torres','ana@mail.com','5551110002','2026-01-12'),
+('Luis Ramírez','luis@mail.com','5551110003','2026-01-15');
+
+INSERT INTO prestamo (id_usuario, id_ejemplar, fecha_prestamo, fecha_devolucion, estado) VALUES
+(1,1,'2026-02-01','2026-02-10','Prestado'),
+(2,3,'2026-02-05','2026-02-15','Prestado');
+
+INSERT INTO reserva (id_usuario, id_ejemplar, fecha_reserva, estado) VALUES
+(3,2,'2026-02-18','Activa'),
+(1,4,'2026-02-18','Activa');
+
+CREATE TABLE historial_prestamos (
+    id_historial INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(100),
+    libro VARCHAR(200),
+    fecha DATE
+);
+
+INSERT INTO historial_prestamos (usuario, libro, fecha)
+SELECT u.nombre, l.titulo, p.fecha_prestamo
+FROM prestamo p
+JOIN usuario u ON p.id_usuario = u.id_usuario
+JOIN ejemplar ej ON p.id_ejemplar = ej.id_ejemplar
+JOIN edicion ed ON ej.id_edicion = ed.id_edicion
+JOIN libro l ON ed.id_libro = l.id_libro;
